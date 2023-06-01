215e3f74dbfa74a83ee9ee9994abca56c1ac9a13
Gaege Root <6657699+GaegeRoot@users.noreply.github.com>
2023-05-31 23:02:04 -0700

    add services page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://farts.cluttercowboys.com',
+    site: 'https://cluttercowboys.com',
 });
