7ebb00853651bdfa1c577838cbc490950a6be65f
Gaege Root <6657699+GaegeRoot@users.noreply.github.com>
2023-06-25 17:14:05 -0700

    add about page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-    site: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://cluttercowboys.com'
+  site: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://cluttercowboys.com',
+  integrations: [sitemap()]
 });
