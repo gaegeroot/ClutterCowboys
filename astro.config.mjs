f3604ca43660d4eae666ff84c815176332f9101a
Gaege Root <6657699+GaegeRoot@users.noreply.github.com>
2024-06-16 13:08:49 -0700

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://cluttercowboys.com',
+  site: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://gaegeroot.github.io',
   integrations: [sitemap()]
 });
