afac3978fd759dfb15f7a989956924534c53a319
Gaege Root <6657699+GaegeRoot@users.noreply.github.com>
2024-06-16 14:18:15 -0700

    fuck

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.NODE_ENV === 'development' ? 'http://localhost:4321' : 'https://cluttercowboys.com',
+  site: process.env.NODE_ENV === 'development' ? 'http://localhost:4321' : 'https://cluttercowboys.onrender.com',
   integrations: [sitemap()]
 });
