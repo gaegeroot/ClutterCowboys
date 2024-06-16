183bd47e272619f16403d096f43a39310a138fa9
Gaege Root <6657699+GaegeRoot@users.noreply.github.com>
2024-06-16 13:32:54 -0700

    add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from 'astro/config';
-
 import sitemap from "@astrojs/sitemap";
 
+// 
 // https://astro.build/config
+// export default defineConfig({
+//   site: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://gaegeroot.github.io',
+//   integrations: [sitemap()]
+// });
+
+import vercel from '@astrojs/vercel/serverless';
 export default defineConfig({
-  site: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://gaegeroot.github.io',
-  integrations: [sitemap()]
+  output: 'server',
+  adapter: vercel()
 });
