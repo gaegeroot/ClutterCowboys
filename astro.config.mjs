c26054376d85a2831aaf197eae164c944d482716
Gaege Root <6657699+GaegeRoot@users.noreply.github.com>
2024-06-16 14:03:28 -0700

    musf

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-//   site: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://gaegeroot.github.io',
+  site: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://astronaut.github.io',
   integrations: [sitemap()]
 });
